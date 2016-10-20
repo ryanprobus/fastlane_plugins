f676ecaca60c79fb55f08f1519c93d0d9bce8bd4
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2016-10-20 15:58:55 -0400

    Add automated tests for check_good_version

--- fastlane-plugin-check_good_version/spec/check_good_version_action_spec.rb
+++ fastlane-plugin-check_good_version/spec/check_good_version_action_spec.rb
@@ -1,9 +1,39 @@
-describe Fastlane::Actions::CheckGoodVersionAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The check_good_version plugin is working!")
+require 'fastlane'
 
-      Fastlane::Actions::CheckGoodVersionAction.run(nil)
+describe Fastlane do
+  describe Fastlane::FastFile do
+    describe "CheckGoodVersionAction" do
+      context "WHEN calling action with the GD.framework installed" do
+        before do
+          allow(Fastlane::Actions::CheckGoodVersionAction).to receive(:sh).with(/xcode-select\s*--print-path/, anything).and_return(File.expand_path('./spec/fixtures/xcodes/xcode_with_good'))
+        end
+
+        it "THEN the version number is returned" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            check_good_version
+          end").runner.execute(:test)
+
+          expect(result).to eq("99.0.1")
+        end
+      end
+
+      context "WHEN calling action with the GD.framework not installed" do
+        before do
+          allow(Fastlane::Actions::CheckGoodVersionAction).to receive(:sh).with(/xcode-select\s*--print-path/, anything).and_return(File.expand_path('./spec/fixtures/xcodes/xcode_without_good'))
+        end
+
+        it "THEN the version number is returned" do
+          check_good_version_no_good_sdk = "lane :test do
+            check_good_version
+          end"
+
+          expect { Fastlane::FastFile.new.parse(check_good_version_no_good_sdk).runner.execute(:test) }.to(
+            raise_error(FastlaneCore::Interface::FastlaneError) do |error|
+              expect(error.message).to match(/The Good framework is not installed/)
+            end
+          )
+        end
+      end
     end
   end
 end
